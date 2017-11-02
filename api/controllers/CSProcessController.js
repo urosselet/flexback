9760a1450e1bd8c39b0a1b8aefcd983bea564161
David L <jean.legrand@he-arc.ch>
2017-11-02 17:07:03 +0100

    Update process edit

--- api/controllers/CSProcessController.js
+++ api/controllers/CSProcessController.js
@@ -8,68 +8,85 @@ let client = sails.config.es.client;
  */
 module.exports = {
 
-	/**
-	 * Find all CS processes
-	 * @param  {[type]} req [description]
-	 * @param  {[type]} res [description]
-	 * @return {[type]}     [description]
-	 */
-	find: function(req, res) {
-        client.search({ 
-            'index': 'operation', 
-            'type': 'cs_process', 
+    /**
+     * Find all CS processes
+     * @param  {[type]} req [description]
+     * @param  {[type]} res [description]
+     * @return {[type]}     [description]
+     */
+    find: function(req, res) {
+        client.search({
+            'index': 'operation',
+            'type': 'cs_process',
             'body': { 'query': { 'match_all': {} } }
         }).then(function(results) {
-
-        	let csprocessArray = [];
-
-        	results.hits.hits.forEach(function(csprocess) {
-
-        		if (typeof csprocess._source.data.dimensions !== 'undefined') {
-
-        			let csprocessObj = {
-	        			'id': csprocess._id,
-	        			'process_name': csprocess._source.process_name,
-	        			'label': csprocess._source.data.label,
-	        			'dimensions': csprocess._source.data.dimensions[0]
-	        		}
-
-	        		csprocessArray.push(csprocessObj);
-        		}
-
-        	});
-            
-            return res.json(csprocessArray);
-
+            return res.json(results.hits.hits);
         });
-	},
+    },
 
-	/**
-	 * Find one CS Process based on it's ID
-	 * @param  {[type]} req [description]
-	 * @param  {[type]} res [description]
-	 * @return {[type]}     [description]
-	 */
-	findOne: function(req, res) {
-		client.get({ index: 'operation', type: 'cs_process', id: req.param('id') })
+    /**
+     * Find one CS Process based on it's ID
+     * @param  {[type]} req [description]
+     * @param  {[type]} res [description]
+     * @return {[type]}     [description]
+     */
+    findOne: function(req, res) {
+        client.get({ index: 'operation', type: 'cs_process', id: req.param('id') })
             .then(function(csprocesses) {
 
-            	let csprocessArray = [];
+                let csprocessArray = [];
 
-            	csprocesses.forEach(function(csprocess) {
-            		let csprocessObj = {
-            			'id': csprocess._id,
-            			'process_name': csprocess._source.data.process_name,
-            			'labels': csprocess._source.data.label,
-            			'dimension': csprocess._source.data.dimensions[0]
-            		}
-            		csprocessArray.push(csprocessArray);
-            	});
-                
+                csprocesses.forEach(function(csprocess) {
+                    let csprocessObj = {
+                        'id': csprocess._id,
+                        'process_name': csprocess._source.data.process_name,
+                        'labels': csprocess._source.data.label,
+                        'dimension': csprocess._source.data.dimensions[0]
+                    }
+                    csprocessArray.push(csprocessArray);
+                });
+
                 return res.json(csprocessArray);
 
             });
-	}
+    },
+
+    /**
+     * Get CS Processes array for wizard
+     * @param  {[type]} req [description]
+     * @param  {[type]} res [description]
+     * @return {[type]}     [description]
+     */
+    wizard: function(req, res) {
+        client.search({
+            'index': 'operation',
+            'type': 'cs_process',
+            'body': { 'query': { 'match_all': {} } }
+        }).then(function(results) {
+            let csprocessArray = [];
+            results.hits.hits.forEach(function(csprocess) {
+                let dimensions = csprocess._source.data.dimensions;
+                if (typeof dimensions !== 'undefined') {
+                    let csprocessObj = {
+                        'id': csprocess._id,
+                        'process_name': csprocess._source.process_name,
+                        'label': csprocess._source.data.label,
+                        'dimensions': dimensions[0]
+                    };
+                    if (dimensions.length > 1) {
+                        let subdimensionArray = [];
+                        dimensions.forEach(function(dimension) {
+                            dimension.subdimensions.forEach(function(subdimension) {
+                                subdimensionArray.push(subdimension);
+                            });
+                        });
+                        csprocessObj['dimensions']['subdimensions'] = subdimensionArray;
+                    }
+                    csprocessArray.push(csprocessObj);
+                }
+            });
+            return res.json(csprocessArray);
+        });
+    }
 
 };
-
